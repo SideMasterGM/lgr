cda24a02e3d77bf734b9dd32a7fa97c9bea33f01
Nikolai Gorchilov <niki@gorchilov.com>
2013-07-16 15:11:01 +0300

    remove unnecessary debug

--- lib/extractors/redis.class.php
+++ lib/extractors/redis.class.php
@@ -5,11 +5,9 @@ namespace rrd\extractors {
 
 		function __construct($connector, $cfg) {
 			$this->redis = $connector;
-//			print_r($cfg);
 		}
 
 		function get($sensor) {
-			echo __METHOD__ . PHP_EOL;
 			$params = preg_split('/\n|\r|\s|\t/', $sensor, NULL, PREG_SPLIT_NO_EMPTY);
 			try {
 				return array_values(call_user_func(array($this->redis, (string) $sensor->attributes()->method), (string) $sensor->attributes()->id, empty($params) ? NULL : $params));
