322db63e73e63d2176336d255ae1c8f306f63403
Nikolai Gorchilov <niki@gorchilov.com>
2014-07-03 00:26:15 +0300

    json extractor supporting exec and file_get_contents sources

---
+++ lib/extractors/json.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace rrd\extractors {
+	class json {
+		private $data = array();
+
+		function __construct($connector, $cfg) {
+			foreach($cfg->children() as $cmd) {
+				$this->data = array_merge_recursive($this->data, json_decode(call_user_func(array($connector, $cmd->getName()), "{$cmd}"), TRUE));
+			}
+		}
+
+		function get($sensor) {
+			$res = array();
+			foreach($sensor->key as $key) {
+				$index = explode('/', $key);
+				$data = &$this->data;
+				foreach($index as $i) {
+					if (!array_key_exists($i, $data)) {
+						echo "FATAL: key '{$key}' not found\n";
+						return FALSE;
+					}
+					$data = &$data[$i];
+				}
+//				echo "{$key}: {$data}\n";
+				$res[] = $data;
+			}
+//			print_r($res);
+			return $res;
+		}
+	}
+}
+?>
